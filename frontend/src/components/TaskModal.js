a0510452a63207fe68b52e5ca77a76d2730a2b69
Alok Raj <alok.raj@jll.com>
2024-08-19 12:40:41 +0530

    refactor frontend by adding custom hooks and service

--- frontend/src/components/TaskModal.js
+++ frontend/src/components/TaskModal.js
@@ -1,7 +1,6 @@
-import React from "react";
-import { Modal, TextField, Button, Box, IconButton } from "@mui/material";
-import UploadIcon from "@mui/icons-material/Upload";
-import { Delete } from "@mui/icons-material";
+import * as React from "react";
+import { Modal, TextField, Button, Box } from "@mui/material";
+import { FileUpload } from "./FileUpload";
 import { formatDateForInput } from "../utils";
 
 const TaskModal = ({
@@ -55,33 +54,7 @@ const TaskModal = ({
         onChange={(e) => handleChange("deadline", e.target.value)}
       />
       {!isEditing && (
-        <>
-          <input
-            accept="application/pdf"
-            style={{ display: "none" }}
-            id="upload-file"
-            type="file"
-            onChange={handleFileChange}
-          />
-          <label htmlFor="upload-file">
-            <Button
-              variant="contained"
-              component="span"
-              startIcon={<UploadIcon />}
-              style={{ marginTop: 16, marginRight: 16 }}
-            >
-              {file ? file.name : "Upload PDF"}
-            </Button>
-            {file && (
-              <IconButton
-                sx={{ paddingTop: 4 }}
-                onClick={() => handleFileChange({ target: { files: [] } })}
-              >
-                <Delete color="error" />
-              </IconButton>
-            )}
-          </label>
-        </>
+        <FileUpload file={file} handleFileChange={handleFileChange} />
       )}
       <Box display="flex" justifyContent="space-between" marginTop={2}>
         <Button onClick={handleClose}>Cancel</Button>
